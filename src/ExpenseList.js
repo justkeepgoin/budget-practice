425081d72bb77d0e04f964439cd3109d5dd6bdb7
justkeepgoin <manage06@naver.com>
2023-08-25 23:46:27 +0900

    feat. css 모듈을 이용한 하단 소비내역 스타일링
    - css 모듈을 이용한 하단 소비내역 스타일링
    - 메뉴 영어->한글 변경 수정

--- src/ExpenseList.js
+++ src/ExpenseList.js
@@ -1,18 +1,19 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
+import styles from "./ExpenseList.module.css";
 
 const ExpenseList = (props) => {
   const expenses = props.expenses;
 
   return (
-    <div className="expense-list">
-      <h2>Expense List</h2>
+    <div className={styles["expense-List-container"]}>
+      <h2>소비 내역</h2>
       {expenses.length === 0 ? (
-        <p>No expenses to display.</p>
+        <p>소비 내역을 추가해주세요.</p>
       ) : (
         <ul>
           {expenses.map((expense, index) => (
-            <li key={index}>
+            <li key={index} className={styles["expense-item"]}>
               <ExpenseItem expense={expense} />
             </li>
           ))}
