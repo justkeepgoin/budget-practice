e6c499166bb706c49b35e7d5ccd3488a671199f3
justkeepgoin <manage06@naver.com>
2023-08-30 22:11:44 +0900

    feat. props, 소비내역 컴포넌트 수정
    - 전달되지 않는 type 항목 제거
    - 소비내역 항목 텍스트 Memo->메모 수정

--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -1,16 +1,14 @@
 import React from "react";
 
 const ExpenseItem = (props) => {
-  const { name, price, type, purchaseDate, memo, wantsToRepurchase } =
-    props.expense;
+  const { name, price, purchaseDate, memo, wantsToRepurchase } = props.expense;
 
   return (
     <div className="expense-item">
       <h3>{name}</h3>
       <p>가격 : {price}원</p>
-      <p>유형 : {type}</p>
       <p>구입 날짜 : {purchaseDate}</p>
-      {memo && <p>Memo: {memo}</p>}
+      {memo && <p>메모: {memo}</p>}
       <p>재구매 의사 : {wantsToRepurchase ? "Yes" : "No"}</p>
     </div>
   );
